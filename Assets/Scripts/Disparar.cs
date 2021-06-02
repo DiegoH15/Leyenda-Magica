15d40d66e9a411f5f8b032f3b5f8ef07602324d0
DiegoH15 <diegorlandohurtado@gmail.com>
2021-06-01 23:29:09 -0500

    Inicio del Proyecto

---
+++ Assets/Scripts/Disparar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{   
+    public GameObject BalaInicio;  
+    public GameObject BalaPrefab;
+    public float timer;
+    public float maxshoot;
+
+    public float BalaVelocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {        
+            GameObject BalaTemporal = Instantiate(BalaPrefab, BalaInicio.transform.position, BalaInicio.transform.rotation) as GameObject;           
+            Rigidbody rb = BalaTemporal.GetComponent<Rigidbody>();  
+            rb.AddForce(transform.forward * BalaVelocidad);
+            Destroy(BalaTemporal, 5.0f);
+        }
+    }
+
+    void Timer()
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxshoot)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+   
+    public void LoadScene(string sceneName)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneName);
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
