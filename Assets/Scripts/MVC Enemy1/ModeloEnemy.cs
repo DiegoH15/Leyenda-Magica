858a854831fda065b083431526a3e81e70dfd68b
Guillermo Barcelli Jordan <g.barcelli.j@gmail.com>
2021-07-06 16:58:04 -0500

    script ControladorEnemy

--- Assets/Scripts/MVC Enemy1/ModeloEnemy.cs
+++ Assets/Scripts/MVC Enemy1/ModeloEnemy.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class ModeloEnemy : MonoBehaviour
 {
-    
+    public float life, maxlife;    
+    public int rutina;
+    public float cronometro;
+    public Quaternion angulo;
+    public float grado;
+    public bool closeToPlayer;
+    public GameObject target;
+    public Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        target = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
--- Assets/Scripts/MVC Enemy1/VistaEnemy.cs
+++ Assets/Scripts/MVC Enemy1/VistaEnemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class VistaEnemy : MonoBehaviour
 {
+    private ModeloEnemy MEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MEnemy = GetComponent<ModeloEnemy>();
     }
+   
+    void UpdateLife()
+    {
+        life.fillAmount = MEnemy.life / MEnemy.maxlife;
 
+        if (MEnemy.life >= MEnemy.maxlife)
+        {
+            MEnemy.life = MEnemy.maxlife;
+        }
+        if (MEnemy.life <= 0)
+        {
+            MEnemy.life = 0;
+            gameObject.SetActive(false);
+        }
+        MEnemy.life += 2 * Time.delta
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateLife();
     }
 }
