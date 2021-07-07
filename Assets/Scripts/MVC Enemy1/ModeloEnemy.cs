816a0b85d88609bf1c314c1d03faa6cb99a4b1cb
Guillermo Barcelli Jordan <g.barcelli.j@gmail.com>
2021-07-07 02:24:03 -0500

    Pausa

--- Assets/Scripts/MVC Enemy1/ModeloEnemy.cs
+++ Assets/Scripts/MVC Enemy1/ModeloEnemy.cs
@@ -4,21 +4,4 @@ using UnityEngine;
 
 public class ModeloEnemy : MonoBehaviour
 {
-    public float life, maxlife;    
-    public int rutina;
-    public float cronometro;
-    public Quaternion angulo;
-    public float grado;
-    public bool closeToPlayer;
-    public GameObject target;
-    public Rigidbody rb;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        target = GameObject.Find("Player");
-    }
-
-   
 }
